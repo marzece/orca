59e4766141dd1092dcd8da27849af076fc727b5c
Eric Marzec <marzece@sas.upenn.edu>
2017-01-26 12:53:27 -0500

    MTC things seem to be working somewhat

--- Source/Objects/Custom Hardware/SNO+/MTC/ORMTCModel.h
+++ Source/Objects/Custom Hardware/SNO+/MTC/ORMTCModel.h
@@ -328,6 +328,9 @@
 
 
 extern NSString* ORMTCModelBasicOpsRunningChanged;
+extern NSString* ORMTCABaselineChanged;
+extern NSString* ORMTCAThresholdChanged;
+extern NSString* ORMTCAConversionChanged;
 extern NSString* ORMTCModelAutoIncrementChanged;
 extern NSString* ORMTCModelUseMemoryChanged;
 extern NSString* ORMTCModelRepeatDelayChanged;
