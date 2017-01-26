ab0cf72294c71a236ad26f1a13343baf91d4b023
Eric Marzec <marzece@sas.upenn.edu>
2017-01-26 12:53:35 -0500

    Delete unused status readout items
    Since data no longer flows through orca these values are not easily
    available any longer. So there's no reason they should remain in code.
    
    Note this does however break the ORVME readout for the MTC. This
    does not affect SNO+ now that the MTC server handles the VME interface.
    But in the unlikely event that we want ORCA to handle the VME stuff
    again this will be a problem

--- Source/Objects/Custom Hardware/SNO+/MTC/ORMTCModel.m
+++ Source/Objects/Custom Hardware/SNO+/MTC/ORMTCModel.m
@@ -121,16 +121,6 @@ static SnoMtcDBInfoStruct dbLookUpTable[kDbLookUpTableSize] = {
 @implementation ORMTCModel
 
 @synthesize
-dataId = _dataId,
-mtcStatusDataId = _mtcStatusDataId,
-mtcStatusGTID = _mtcStatusGTID,
-mtcStatusCnt10MHz = _mtcStatusCnt10MHz,
-mtcStatusTime10Mhz = _mtcStatusTime10Mhz,
-mtcStatusReadPtr = _mtcStatusReadPtr,
-mtcStatusWritePtr = _mtcStatusWritePtr,
-mtcStatusDataAvailable = _mtcStatusDataAvailable,
-mtcStatusNumEventsInMem = _mtcStatusNumEventsInMem,
-resetFifoOnStart = _resetFifoOnStart,
 pulserEnabled = _pulserEnabled,
 tubRegister;
 
@@ -193,7 +183,6 @@ tubRegister;
 {
     [mtc release];
     [mtcDataBase release];
-    [_mtcStatusTime10Mhz release];
     [super dealloc];
 }
 
